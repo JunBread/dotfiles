fbdbe42203c3336bd552f03c97fe0c57fbff2d77
Junhyun Kim <me@junbread.win>
2020-06-29 01:09:34 +0900

    Vim 설정 스크립트 Vundle 설치, Molokai 다운로드, Plugin 설치 자동화

--- setup-vim.sh
+++ setup-vim.sh
@@ -2,6 +2,8 @@
 
 vimd=~/.vim
 vimrc=~/.vimrc
+vundle=~/.vim/bundle/Vundle.vim
+molokai=~/.vim/colors/molokai.vim
 
 # backup old .vimrc
 if [ -e $vimrc ]; then
@@ -9,6 +11,19 @@ if [ -e $vimrc ]; then
 fi
 
 mkdir -p ~/.vim/include
+mkdir -p ~/.vim/colors
+
+# install vundle
+if [ ! -e $vundle ]; then
+	git clone https://github.com/VundleVim/Vundle.vim.git $vundle
+fi
+
+# install molokai color scheme
+if [ ! -e $molokai ]; then
+wget -O $molokai https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
+fi
 
 ln -sf $(pwd)/vimrc $vimrc
 ln -sf $(pwd)/vim-include ~/.vim/vim-include
+
+vim -c PluginInstall -c q -c q
