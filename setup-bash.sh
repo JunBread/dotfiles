b0a6a92fc84fc6343255c1d15a87fb74d0609a7d
Junhyun Kim <me@junbread.win>
2020-07-07 20:13:53 +0900

    bash 설치 스크립트 --simple 옵션 추가

--- setup-bash.sh
+++ setup-bash.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash 
 
-bashrc=~/.bashrc
+bashrc_source=$(pwd)/bashrc
+bashrc_target=~/.bashrc
+
+if [[ $1 == '--simple' ]]; then
+	bashrc_source=$(pwd)/bashrc.simple
+else
+	# install utilities
+
+	# install bat
+	if [[ ! -f /usr/bin/bat && ! -f /usr/bin/batcat ]]; then
+		sudo apt install -y bat
+	fi
+fi
 
 # backup old .vimrc
-if [[ -f $bashrc && ! -L $bashrc ]]; then
-	mv $bashrc $bashrc.old
+if [[ -f $bashrc_target && ! -L $bashrc_target ]]; then
+	mv $bashrc_target $bashrc_target.old
 fi
 
-# install bat
-if [[ ! -f /usr/bin/bat && ! -f /usr/bin/batcat ]]; then
-	sudo apt install -y bat
-fi
+ln -sfn $bashrc_source $bashrc_target
 
-ln -sf $(pwd)/bashrc $bashrc
+source ~/.bashrc
