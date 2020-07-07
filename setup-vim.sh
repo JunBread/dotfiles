93dd005f6051273266302b40e7796e0b3da9acd4
Junhyun Kim <me@junbread.win>
2020-07-07 19:42:00 +0900

    파일 백업 스크립트 오류 수정

--- setup-vim.sh
+++ setup-vim.sh
@@ -9,29 +9,30 @@ vimrc_target=~/.vimrc
 viminclude_source=$(pwd)/vim-include
 viminclude_target=$vimd/vim-include
 
-if [ $1 == 'simple' ]; then
+if [[ "$1" == "--simple" ]]; then
 	viminclude_source=$(pwd)/vim-include/simple
 fi
 
 # backup old .vimrc
-if [ -e $vimrc_target ]; then
+if [[ -f $vimrc_target && ! -L $vimrc_target ]]; then
 	mv $vimrc_target $vimrc_target.old
 fi
 
 mkdir -p ~/.vim/include
 mkdir -p ~/.vim/colors
 
-# install vundle
-if [ ! -e $vundle ]; then
+# install vundle.
+# note that it is directory
+if [[ ! -d $vundle ]]; then
 	git clone https://github.com/VundleVim/Vundle.vim.git $vundle
 fi
 
 # install monokai color scheme
-if [ ! -e $monokai ]; then
+if [[ ! -f $monokai ]]; then
 	wget -O $monokai https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim
 fi
 
-ln -sf $vimrc_source $vimrc_target
-ln -sf $viminclude_source $viminclude_target
+ln -sfn $vimrc_source $vimrc_target
+ln -sfn $viminclude_source $viminclude_target
 
 vim -c PluginInstall -c q -c q
