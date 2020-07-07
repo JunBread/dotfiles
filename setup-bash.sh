b47e87ddd630785a129824f8195b9cdca8504e03
Junhyun Kim <me@junbread.win>
2020-07-07 19:50:55 +0900

    bash util 설치 추가

--- setup-bash.sh
+++ setup-bash.sh
@@ -7,4 +7,9 @@ if [[ -f $bashrc && ! -L $bashrc ]]; then
 	mv $bashrc $bashrc.old
 fi
 
+# install bat
+if [[ ! -f /usr/bin/bat && ! -f /usr/bin/batcat ]]; then
+	sudo apt install -y bat
+fi
+
 ln -sf $(pwd)/bashrc $bashrc
