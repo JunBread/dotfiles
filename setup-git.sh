19f242e140795293da3fd753599e3d61335b9760
Junhyun Kim <me@junbread.win>
2020-07-09 23:46:30 +0900

    git local 설정 분리, 설치 스크립트 --simple 옵션 추가

--- setup-git.sh
+++ setup-git.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash 
 
-ln -sf $(pwd)/gitconfig ~/.gitconfig
-ln -sf $(pwd)/gitexclude ~/.gitexclude
+# copy predefined local gitconfig unless simple flag is enabled
+if [[ $1 != "--simple" ]]; then
+    cp $(pwd)/gitconfig.local ~/.gitconfig.local
+fi
+
+# make local gitconfig
+touch ~/.gitconfig.local
+
+# symlink gitconfig
+ln -sfn $(pwd)/gitconfig ~/.gitconfig
+ln -sfn $(pwd)/gitexclude ~/.gitexclude
