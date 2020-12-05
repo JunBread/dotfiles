c9ababb0954102704653669d89419c977e36170a
Junhyun Kim <me@junbread.win>
2020-12-05 18:45:32 +0900

    Mac 환경에서 bat 설치시 brew 이용하도록 변경

--- setup-bash.sh
+++ setup-bash.sh
@@ -5,26 +5,26 @@ BASHRC_TARGET=~/.bashrc
 BASHRC_LOCAL_SOURCE=$(pwd)/bashrc.local
 BASHRC_LOCAL_TARGET=~/.bashrc.local
 
-if [ $1 != "--simple" ]; then
+if [[ $1 != "--simple" ]]; then
     # copy initial local .bashrc
     cp $BASHRC_LOCAL_SOURCE $BASHRC_LOCAL_TARGET
 
     # install bat
     if ! command -v bat &>/dev/null && ! command -v batcat &>/dev/null; then
-      if [ "$OSTYPE" == "linux"* ]; then
+      if [[ "$OSTYPE" == "linux"* ]]; then
         sudo apt install -y bat
-      elif [ "$OSTYPE" == "darwin"* ]; then
+      elif [[ "$OSTYPE" == "darwin"* ]]; then
         brew install bat
-	  	fi
+	  fi
     fi
 
     # install nvm
     export PROFILE=$BASHRC_LOCAL_TARGET
-    wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 fi
 
 # backup old bashrc
-if [ -f $BASHRC_TARGET ] && [ ! -L $BASHRC_TARGET ]; then
+if [[ -f $BASHRC_TARGET && ! -L $BASHRC_TARGET ]]; then
     mv $BASHRC_TARGET $BASHRC_TARGET.old
 fi
 
